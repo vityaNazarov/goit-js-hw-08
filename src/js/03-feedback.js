3e1c90ab693c7be7bb14cfef4595daece060096c
Viktor <108054291+vityaNazarov@users.noreply.github.com>
2022-11-30 16:35:25 +0200

    qwqwe

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,44 +1,15 @@
-// import throttle from 'lodash.throttle';
-// const refs = {
-//   form: document.querySelector('.feedback-form'),
-//   email: document.querySelector('input'),
-//   message: document.querySelector('textarea'),
-//   button: document.querySelector('button'),
-// };
-// refs.form.addEventListener('submit', onButtonClick);
-// refs.form.addEventListener('input', throttle(onInputClick, 500));
-
-// const LOCALSTORAGE_KEY = 'feedback-form-state';
-// const formDate = {};
-
-// function onButtonClick(e) {
-//   e.preventDefault();
-//   if (refs.email.value === '') {
-//     alert('всі поля повинні бути заповнені!');
-//   } else if (refs.message.value === '') {
-//     alert('всі поля повинні бути заповнені!');
-//   } else {
-//     localStorage.removeItem(LOCALSTORAGE_KEY);
-//     console.log(formDate);
-//     e.currentTarget.reset();
-//   }
-// }
-
-// function onInputClick(e) {
-//   formDate[e.target.name] = e.target.value;
-//   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formDate));
-// }
-// if (localStorage.getItem(LOCALSTORAGE_KEY)) {
-//   console.log(localStorage);
-// }
-
 import throttle from 'lodash.throttle';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input'),
+  message: document.querySelector('textarea'),
+  button: document.querySelector('button'),
+};
 
 const STORAGE_KEY = 'feedback-form-state';
 const formData = {};
-
 const formEl = document.querySelector('form');
-
 formEl.addEventListener('submit', onFormSubmit);
 formEl.addEventListener('input', throttle(onformEl, 500));
 
@@ -53,7 +24,13 @@ function onFormSubmit(e) {
 
   console.log({ Email: email.value, Message: message.value });
 
-  formEl.reset();
+  if (refs.email.value === '') {
+    alert('всі поля повинні бути заповнені!');
+  } else if (refs.message.value === '') {
+    alert('всі поля повинні бути заповнені!');
+  } else {
+    e.currentTarget.reset();
+  }
   localStorage.removeItem(STORAGE_KEY);
 }
 
@@ -64,10 +41,8 @@ function onformEl(e) {
 
 function fillForm() {
   const savedForm = localStorage.getItem(STORAGE_KEY);
-
   if (savedForm) {
     const parceSavedForm = JSON.parse(savedForm);
-
     const keys = Object.keys(parceSavedForm);
     for (const key of keys) {
       formEl.elements[key].value = parceSavedForm[key];
